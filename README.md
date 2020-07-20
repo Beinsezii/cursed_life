796850e04755c192e43e7bb6c74de8a090c54f33
Beinsezii <beinsezii@gmail.com>
2020-07-19 18:52:54 -0700

    99% Crossterm port. Still no resize.
    basically just made some extra macros and fns to switch everything over
    to pure Crossterm. program ain't complicated enough to warrant TUI or
    Cursive. Seems it's also siginificantly faster yet again since I now
    only manually erase in certain scenarios instead of every frame.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# cursed_life 0.13.2
-basically game of life in ncurses. doesn't use cutting-edge algorithms. more of a small Rust exercise than anything.
+# cursed_life 0.14.0
+basically game of life in tui. doesn't use cutting-edge algorithms. more of a small Rust exercise than anything.
+
+first version was based in ncurses, hence the name.
 
 <img width=720 src="./thick_screenshot.png" />
 <img width=400 src="./controls.png" />
@@ -10,4 +12,4 @@ Just run the file in a terminal, use 'h' to show/hide controls.
 Don't resize the window; that's not implemented yet.
 
 ## Building
-Uses `pancurses` crate which claims to be platform-agnostic. Can guarantee it works in Linux.
+Uses `crossterm` crate which claims to be platform-agnostic. Can guarantee it works in Linux.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-use pancurses::Input;
 use rayon::prelude::*;
+use std::io::{Write, stdout};
+use std::time::{Duration, Instant};
+use crossterm::{
+    ExecutableCommand, QueueableCommand,
+    queue,
+    terminal,
+    event::{Event, KeyEvent, KeyCode, KeyModifiers, read, poll},
+    cursor,
+    style::Print
+};
 
 
 //// Logic FNs ////
 
 // creates a new grid of x/y size optionally taking extra data from another grid
-fn gen_grid(x: usize, y: usize, grid: Option<Vec<Vec<bool>>>) ->  Vec<Vec<bool>> {
+fn gen_grid(cols: usize, rows: usize, grid: Option<Vec<Vec<bool>>>) ->  Vec<Vec<bool>> {
     match grid {
         Some(mut data) => {
             for col in &mut data{
-                col.resize(x, false)
+                col.resize(cols, false)
             }
-            data.resize(y, vec![false; x]);
+            data.resize(rows, vec![false; cols]);
             data
         }
-        None => vec![vec![false; x]; y],
+        None => vec![vec![false; cols]; rows],
     }
 }
 
@@ -101,18 +110,38 @@ fn grid_to_str(grid: &Vec<Vec<bool>>, char_true: char, char_false: char) -> Stri
 }
 
 
-// clears window and redraws text.
-fn redraw(window: &pancurses::Window, text: &str) {
-    let (cy, cx) = window.get_cur_yx();
-    window.erase();
-    window.addstr(text);
-    window.mv(cy, cx);
+// clears terminal and redraws text.
+fn redraw<T: Write>(buff: &mut T, text: &str) {
+    buff.queue(cursor::SavePosition).unwrap();
+    let mut i = 0;
+    for slice in text.split('\n') {
+        buff.queue(cursor::MoveTo(0, i)).unwrap()
+            .queue(Print(slice)).unwrap();
+        i += 1;
+    }
+    buff.queue(cursor::RestorePosition)
+        .unwrap()
+        .flush()
+        .unwrap();
 }
 
 
 // returns true if char is an acceptable display character
 fn valid_chars(c: char) -> bool{
     c.is_alphanumeric() || c.is_whitespace() || c.is_ascii_punctuation()
+}
+
+
+// get crossterm event with optional poll duration.
+fn get_event(duration: Option<Duration>) -> Option<Event>{
+    match duration {
+        Some(delay) => {
+            if poll(delay).unwrap() {
+                Some(read().unwrap())
+            } else {None}
+        },
+        None => Some(read().unwrap())
+    }
 }
 
 
@@ -135,134 +164,181 @@ comma/period ',.' : adjust max framerate
 c                 : change dispaly characters";
 
 
+// key event shorthand. Can match get_event to KE!(char)
+macro_rules! KE {
+    ($ch:expr) => {
+        Event::Key(KeyEvent{code: KeyCode::Char($ch), modifiers: _})
+    };
+    ($ch:expr, $mod:expr) => {
+        Event::Key(KeyEvent{code: KeyCode::Char($ch), modifiers: $mod})
+    }
+}
+
+
 fn main() {
+    terminal::enable_raw_mode().unwrap();
+    let (mut cols, mut rows) = terminal::size().unwrap();
+    let mut stdo = stdout();
+
+    queue!(
+        stdo,
+        terminal::EnterAlternateScreen,
+        cursor::MoveTo(cols/2, rows/2),
+        ).unwrap();
+
     let log: bool;
     match std::env::args().find(|x| x == "-l") {
         Some(_) => log = true,
         None => log = false,
     }
+
     let mut ch_t = 'O';
     let mut ch_f = ' ';
     let mut live: i32 = 2;
     let mut birth: i32 = 3;
     let framerates = [0.5, 1., 2., 5., 10., 15., 20., 30., 45., 60., 90., 120., 999.];
     let mut framerate = 5;
-    let mut window = pancurses::initscr();
-    pancurses::noecho();
-    let (mut cols, mut rows) = window.get_max_yx();
 
-    let mut matrix = gen_grid(rows as usize, cols as usize - 1, None);
+    let mut matrix = gen_grid(cols as usize, rows as usize - 1, None);
 
     let mut draw_times = Vec::<u128>::new();
     let mut step_times = Vec::<u128>::new();
 
+    // advance the game one iter
     macro_rules! step {
         () => {
             matrix = gol_step(&matrix, live, birth);
         }
     }
 
+    // redraw the game and toolbar
     macro_rules! redraw_all {
         () => {
-            redraw(&window, &(grid_to_str(&matrix, ch_t, ch_f)+&gen_toolbar(ch_t, ch_f, live, birth, framerates[framerate])));
+            redraw(&mut stdo,
+                   &(grid_to_str(&matrix, ch_t, ch_f) +
+                   &gen_toolbar(ch_t, ch_f, live, birth, framerates[framerate])));
         }
     }
 
+    // blank the screen
+    macro_rules! erase {
+        () => {
+            let blank = String::from(" ").repeat((cols*rows).into());
+            redraw(&mut stdo, &blank);
+        }
+    }
+
+    // erase!(), write HELP_TEXT, wait for keycode 'h', redraw_all!()
     macro_rules! show_help {
         () => {
-            pancurses::curs_set(0);
-            redraw(&window, HELP_TEXT);
-            while window.getch() != Some(Input::Character('h')) {}
-            pancurses::curs_set(1);
+            stdo.queue(cursor::Hide).unwrap();
+            erase!();
+            redraw(&mut stdo, HELP_TEXT);
+            loop {
+                match get_event(None) {
+                    Some(KE!('h')) => break,
+                    _ => (),
+                }
+            }
+            stdo.queue(cursor::Show).unwrap();
             redraw_all!();
         }
     }
 
-    window.mv(cols/2, rows/2);
+    // start off with control screen since there's no cmd args besides -l
     show_help!();
 
+    // main loop
     loop {
-        let (cur_row, cur_col) = window.get_cur_yx();
+        let (cur_col, cur_row) = cursor::position().unwrap();
 
-        match window.getch() {
+        match get_event(None) {
             // movement
-            Some(Input::Character('w')) => {window.mv(cur_row-1, cur_col);},
-            Some(Input::Character('a')) => {window.mv(cur_row, cur_col-1);},
-            // make sure it doesn't clip toolbar. Laziest way possible.
-            // TODO maybe the main win should be 1 less than window, and toolbar should be its own win?
-            Some(Input::Character('s')) => {window.mv(
-                std::cmp::min(cur_row+1, matrix.len() as i32 - 1), cur_col);},
-            Some(Input::Character('d')) => {window.mv(cur_row, cur_col+1);},
+            Some(KE!('w')) => {stdo.queue(cursor::MoveUp(1)).unwrap();},
+            Some(KE!('a')) => {stdo.queue(cursor::MoveLeft(1)).unwrap();},
+            Some(KE!('s')) => {
+                // prevent selecting the toolbar which is invalid matrix bounds
+                if cur_row < rows - 2 {stdo.queue(cursor::MoveDown(1)).unwrap();}
+            },
+            Some(KE!('d')) => {stdo.queue(cursor::MoveRight(1)).unwrap();},
 
             // toggle point
-            Some(Input::Character(' ')) => {
+            Some(KE!(' ')) => {
                 grid_toggle(&mut matrix, cur_col as usize, cur_row as usize);
                 redraw_all!();
             },
 
             // change rules
-            Some(Input::Character('-')) => {
+            Some(KE!('-')) => {
                 live = (live-1).max(0);
                 redraw_all!();
             },
-            Some(Input::Character('=')) => {
+            Some(KE!('=')) => {
                 live = (live+1).min(9);
                 redraw_all!();
             },
-            Some(Input::Character('[')) => {
+            Some(KE!('[')) => {
                 birth = (birth-1).max(0);
                 redraw_all!();
             },
-            Some(Input::Character(']')) => {
+            Some(KE!(']')) => {
                 birth = (birth+1).min(9);
                 redraw_all!();
             },
 
             // frame-advance
-            Some(Input::Character('e')) =>  {
+            Some(KE!('e')) =>  {
                 step!();
                 redraw_all!();
             }
 
             // change framerate
-            Some(Input::Character(',')) => {
+            Some(KE!(',')) => {
                 framerate = (framerate as i32-1).max(0) as usize;
                 redraw_all!();
             }
-            Some(Input::Character('.')) => {
+            Some(KE!('.')) => {
                 framerate = (framerate+1).min(framerates.len()-1);
                 redraw_all!();
             }
 
-            // play
-            Some(Input::Character('f')) =>  {
-                pancurses::curs_set(0);
-                window.timeout(0);
-                let max_delay = ((1./framerates[framerate]) * 1000000.) as i32;
-                while window.getch() != Some(Input::Character('f')){
-                    let now = std::time::Instant::now();
+            // play. also logs performance if -l passed.
+            Some(KE!('f')) =>  {
+                stdo.queue(cursor::Hide).unwrap();
+                let min_delay = Duration::from_micros(1);
+                let max_delay = Duration::from_secs_f64(1./framerates[framerate]);
+                let mut poll_time = min_delay;
+                let mut delta: Duration;
+                while get_event(Some(poll_time)) != Some(Event::Key(
+                    KeyEvent{
+                        code: KeyCode::Char('f'),
+                        modifiers: KeyModifiers::empty()
+                    }))
+                {
+                    let total_timer = Instant::now();
                     if log {
-                        let step_timer = std::time::Instant::now();
+                        let step_timer = Instant::now();
                         step!();
                         step_times.push(step_timer.elapsed().as_micros());
-                        let draw_timer = std::time::Instant::now();
+                        let draw_timer = Instant::now();
                         redraw_all!();
                         draw_times.push(draw_timer.elapsed().as_micros());
                     } else {
                         step!();
                         redraw_all!();
                     }
-                    window.timeout(((max_delay - now.elapsed().as_micros() as i32) / 1000).max(0));
+                    delta = total_timer.elapsed();
+                    poll_time = if max_delay > delta {max_delay - delta}
+                                else {min_delay}
                 }
-                window.timeout(-1);
-                pancurses::curs_set(1);
+                stdo.queue(cursor::Show).unwrap();
             }
 
             // clear
-            Some(Input::Character('x')) => {
-                match window.getch() {
-                    Some(Input::Character('x')) => {
-                        matrix = gen_grid(rows as usize, cols as usize - 1, None);
+            Some(KE!('x')) => {
+                match get_event(None) {
+                    Some(KE!('x')) => {
+                        matrix = gen_grid(cols as usize, rows as usize - 1, None);
                         redraw_all!();
                     },
                     _ => (),
@@ -270,59 +346,62 @@ fn main() {
             }
 
             // change chars
-            Some(Input::Character('c')) => {
-                window.mv(cols-1, 4);
-                match window.getch() {
-                    Some(Input::Character(c)) => {
-                        if valid_chars(c) && c != ch_f {ch_t = c;}
+            Some(KE!('c')) => {
+                stdo.execute(cursor::MoveTo(4, rows-1)).unwrap();
+                match get_event(None) {
+                    Some(Event::Key(KeyEvent{code: key, modifiers: _})) => match key {
+                        KeyCode::Char(c) => if valid_chars(c) && c != ch_f {ch_t = c;},
+                        _ => (),
                     }
-                    _ => ()
+                    _ => (),
                 }
                 redraw_all!();
-                window.mv(cols-1, 11);
-                match window.getch() {
-                    Some(Input::Character(c)) => {
-                        if valid_chars(c) && c != ch_t {ch_f = c;}
+                stdo.execute(cursor::MoveTo(11, rows-1)).unwrap();
+                match get_event(None) {
+                    Some(Event::Key(KeyEvent{code: key, modifiers: _})) => match key {
+                        KeyCode::Char(c) => if valid_chars(c) && c != ch_t {ch_f = c;},
+                        _ => (),
                     }
-                    _ => ()
+                    _ => (),
                 }
-                window.mv(cur_row, cur_col);
+                stdo.execute(cursor::MoveTo(cur_col, cur_row)).unwrap();
                 redraw_all!();
             }
 
             // show/hide help.
-            Some(Input::Character('h')) => {
+            Some(KE!('h')) => {
                 show_help!();
             }
 
             // TODO resize. Seems like it's missing a lot of resize fns in the docs.
             // Blocking issue for 1.0
-            Some(Input::KeyResize) => {
-                window.clear();
-                window.refresh();
-                // window.delwin();
-                pancurses::resize_term(0, 0);
-                window = pancurses::newwin(0, 0, 0, 0);
+            // Some(Input::KeyResize) => {
+            //     window.clear();
+            //     window.refresh();
+            //     // window.delwin();
+            //     pancurses::resize_term(0, 0);
+            //     window = pancurses::newwin(0, 0, 0, 0);
 
-                cols = window.get_max_y();
-                rows = window.get_max_x();
-                matrix = gen_grid(rows as usize, cols as usize - 1, Some(matrix));
-                redraw_all!();
-            }
+            //     cols = window.get_max_y();
+            //     rows = window.get_max_x();
+            //     matrix = gen_grid(rows as usize, cols as usize - 1, Some(matrix));
+            //     redraw_all!();
+            // }
 
             // quit
-            Some(Input::Character('q')) => {
-                match window.getch() {
-                    Some(Input::Character('q')) => break,
-                    _ => (),
-                }
-            }
+            Some(KE!('q')) => match get_event(None) {
+                Some(KE!('q')) => break,
+                _ => (),
+            },
 
             _ => (),
         }
     }
 
-    pancurses::endwin();
+    // cleanup
+    stdo.queue(terminal::LeaveAlternateScreen).unwrap();
+    terminal::disable_raw_mode().unwrap();
+
     if log {
         step_times.sort();
         draw_times.sort();
